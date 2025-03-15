d54b467978922a73c4270d069b343441ea827cc6
ghostpete <ghostcoderme@gmail.com>
2025-03-15 15:15:27 +0100

    added new db instance

--- RayversKitchenAPI/settings.py
+++ RayversKitchenAPI/settings.py
@@ -124,18 +124,18 @@ WSGI_APPLICATION = "RayversKitchenAPI.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=config("DATABASE_URL")
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL")
+    )
+}
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
